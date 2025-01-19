9ccb1d9d29f2fdc38aa517db54b896091c470db1
Jakub Woźniak <jakub.wozniak@nordcloud.com>
2025-01-19 22:41:03 +0100

    better handling for multi arch configurations I guess

--- flake.nix
+++ flake.nix
@@ -7,19 +7,35 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
-    homeConfigurations."jakub@armisael" = let
-      system = "aarch64-darwin";
-      hostname = "armisael";
-    in home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.${system};
-      modules = [
-        ./home/common.nix
-        ./home/arch_${system}.nix
-        ./home/host_${hostname}.nix
-      ];
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      managedHosts = {
+        armisael = { # personal laptop
+          system = "aarch64-darwin";
+          hostname = "armisael";
+        };
+        zeruel = { # work laptop
+          system = "aarch64-darwin";
+          hostname = "zeruel";
+        };
+      };
+    in {
+      homeConfigurations = builtins.listToAttrs (map (hostName: {
+        name = "jakub@${hostName}";
+
+        value = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs { system = managedHosts.${hostName}.system; };
+
+          modules = [
+            ./home/common.nix
+            ./home/arch_${managedHosts.${hostName}.system}.nix
+            ./home/host_${hostName}.nix
+          ];
+        };
+      }) (builtins.attrNames managedHosts));
+
     };
-  };
 }
