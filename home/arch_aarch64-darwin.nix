ecf1ac9368ae0e06139f786edcd07fc4e9f77c62
Jakub Woźniak <jakub.wozniak@nordcloud.com>
2025-01-21 20:26:08 +0100

    fix path for Ghostty's config

--- home/arch_aarch64-darwin.nix
+++ home/arch_aarch64-darwin.nix
@@ -4,9 +4,8 @@
   # Example: homebrew or special macOS-related packages
   home.packages = with pkgs; [ ];
 
-  home.file."~/Library/Application support/com.mitchellh.ghostty/config".source =
+  home.file."Library/Application\ Support/com.mitchellh.ghostty/config".source =
     ../ghostty.conf;
-
   home.homeDirectory = "/Users/jakub";
 }
 
